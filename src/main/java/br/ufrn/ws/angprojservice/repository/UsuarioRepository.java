e665ceac4ed40fe61444dcfe152599b8ab107fc5
angelelouise <angelealst@hotmail.com>
2018-06-03 13:20:23 -0300

    adicionando querys de findByUser e findByNome

--- src/main/java/br/ufrn/ws/angprojservice/repository/UsuarioRepository.java
+++ src/main/java/br/ufrn/ws/angprojservice/repository/UsuarioRepository.java
@@ -1,9 +1,18 @@
 package br.ufrn.ws.angprojservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.ufrn.ws.angprojservice.dominio.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	@Query(value= "SELECT * FROM usuario u WHERE u.user = user LIMIT 1", nativeQuery=true)
+	public Usuario findByUser(String user);
+	
+	@Query(value= "SELECT * FROM usuario u WHERE LOWER(u.nome) = LOWER(nome)", nativeQuery=true)
+	public List<Usuario> findByNome (String nome);
 
 }
--- src/main/java/br/ufrn/ws/angprojservice/resource/UsuarioResource.java
+++ src/main/java/br/ufrn/ws/angprojservice/resource/UsuarioResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +40,10 @@ public class UsuarioResource {
 	@GetMapping("/{idUsuario}")//busca usuario por id
 	public Usuario listarId (@PathVariable Long idUsuario) {
 		return usuarioRepository.findById(idUsuario).get();
+	}
+	@GetMapping("list/{user}")//busca usuario por user
+	public Usuario listarUser (@PathVariable String user) {
+		return  (Usuario) usuarioRepository.findByUser(user);
 	}
 	@PutMapping//atualizando um usuario existente
 	@ResponseStatus(code=HttpStatus.OK)
