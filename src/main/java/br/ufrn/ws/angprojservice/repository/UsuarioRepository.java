e58b9eb960fcb398421914eb1926c7a6d037a092
angelelouise <angelealst@hotmail.com>
2018-06-09 22:11:12 -0300

    consulta do usuario corrigida

--- src/main/java/br/ufrn/ws/angprojservice/repository/UsuarioRepository.java
+++ src/main/java/br/ufrn/ws/angprojservice/repository/UsuarioRepository.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.ufrn.ws.angprojservice.dominio.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	@Query(value= "SELECT * FROM usuario u WHERE u.usuario = usuario LIMIT 1", nativeQuery=true)
-	public Usuario findByUser(String usuario);
+	@Query(value= "SELECT * FROM public.usuario u WHERE u.usuario = :usuario LIMIT 1", nativeQuery=true)
+	public Usuario findByUser(@Param("usuario") String usuario);
 	
-	@Query(value= "SELECT * FROM usuario u WHERE LOWER(u.nome) = LOWER(nome)", nativeQuery=true)
-	public List<Usuario> findByNome (String nome);
+	@Query(value= "SELECT * FROM public.usuario u WHERE LOWER(u.nome) = LOWER(:nome)", nativeQuery=true)
+	public List<Usuario> findByNome (@Param("nome")String nome);
 
 }
